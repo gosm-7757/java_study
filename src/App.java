062f3a1e60071f52962da3d681905c96452430d7
gosm-7757 <gogosm7757@naver.com>
2023-09-18 20:20:06 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int num = 10;
+        System.out.println(num);
+    }
+}
